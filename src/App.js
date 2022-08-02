0ded2b6f62c524dfe932b6613b3120f4c71a7387
queuing4oranges <katjazenker@gmail.com>
2022-08-02 17:46:10 +0200

    links and navlinks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import './App.css'
+import './App.css'; 
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+
+//page components:
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <nav>
         <h1>My Articles</h1>
+
+        {/* using exact, so / isnt included into /about etc. */}
+        <NavLink exact to="/">Home</NavLink> 
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+
       </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route>
+          <Contact exact path="/contact"/>
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2>Homepage</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sapiente voluptatum dolorem, 
+          voluptate veritatis quasi cum ipsa, natus maxime distinctio, ipsum fugiat eos! Cupiditate, 
+          quibusdam. Autem delectus alias impedit quibusdam.</p>
+    </div>
+  )
+}
