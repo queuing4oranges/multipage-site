21e37c0cad4dd00f170a09cb4957fdd2e8fd7a31
queuing4oranges <katjazenker@gmail.com>
2022-08-09 15:20:31 +0200

    query params

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -7,15 +7,15 @@ export default function Article() {
     const { id } = useParams()
     const url = 'http://localhost:3000/articles/'+id     //concatenating id of specific article
     const { data: article, isPending, error } = useFetch(url)
-    const history = useHistory()        //we need to invoke the fct so it can return an object to us that we can than use
+    const history = useHistory()                            //invoke the fct so it can return an object to us that we can than use
   
-    useEffect(() =>{        //function will run once in the beginning, but we only want to redirect when there actually is an error
+    useEffect(() =>{                                        //function will run once in the beginning, but we only want to redirect when there actually is an error
         if(error){
             //redirect
-            //history.goBack()      //one possiblity, but we'll be using an alternative
+            //history.goBack()                              //one possiblity, but we'll be using an alternative
             setTimeout(() => {history.push('/')}, 2000)     //setting timeout so user can read message           
         }
-    }, [error])
+    }, [error, history])
 
   return (
     <div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 export default function Contact() {
+
+  //gets query string from path in browser:
+  const queryString = useLocation().search
+  console.log(queryString)
+
+  //making new object, so we can use the data from above
+  const queryParams = new URLSearchParams(queryString)
+
+  //on that object we have a specific GET method to get a specific parameter
+  const name = queryParams.get("name")
+
+  //we can output that also in the browser like this:
   return (
     <div>
-        <h2>Contact</h2>
+        <h2>Hey {name}, Contact us here...</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sapiente voluptatum dolorem, 
           voluptate veritatis quasi cum ipsa, natus maxime distinctio, ipsum fugiat eos! Cupiditate, 
           quibusdam. Autem delectus alias impedit quibusdam.</p>
